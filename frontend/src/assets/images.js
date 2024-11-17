06356e6096dabd6b087c81c5b9eacc98b4313a69
istekhar <istekharkhan46@gmail.com>
2024-11-17 14:19:43 +0530

    first commit

---
+++ frontend/src/assets/images.js
@@ -0,0 +1,12 @@
+import image_1 from './images/1.jpg';
+import image_2 from './images/2.jpg';
+import image_3 from './images/3.jpg';
+import image_4 from './images/4.jpg';
+
+
+export const imagesUrls = {
+    url_1 : image_1,
+    url_2 : image_2,
+    url_3 : image_3,
+    url_4 : image_4,
+}
